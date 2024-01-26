eac3c3dc13b6ad3ce6210a69179673d0c1f7b419
Tofan <tofanalin1@gmail.com>
2024-01-26 17:06:54 +0200

    First commit

---
+++ views/login.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include("includes/head-tag-content.php")?>
+</head>
+<body>
+
+<div class="h-100 d-flex align-items-center justify-content-center">
+
+</div>
+
+<?php include("includes/footer-tag-content.php")?>
+</body>
+</html>
