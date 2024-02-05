17720cd9ed135696c1211699a4d26a1256f1f7e5
Tofan <tofanalin1@gmail.com>
2024-02-05 10:16:55 +0200

    created new tables

--- app/database/create-tables.php
+++ app/database/create-tables.php
@@ -24,3 +24,82 @@ function createUsersTable()
 
     disconnect($connection);
 }
+
+function createExpensesTable()
+{
+    $connection = connectDefaultDatabase();
+
+    $sql = "CREATE TABLE IF NOT EXISTS expenses (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            value FLOAT(6,2) NOT NULL,
+            note VARCHAR(100) NOT NULL,
+            date DATETIME NOT NULL,
+            category_id INT(6) UNSIGNED NOT NULL, 
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+            )";
+
+    if ($connection->query($sql) === TRUE) {
+        echo 'The expenses table was created successfully.';
+    } else {
+        echo 'There is a problem with the database, please contact an administrator.';
+    }
+
+    disconnect($connection);
+}
+
+function createCategoriesTable()
+{
+    $connection = connectDefaultDatabase();
+
+    $sql = "CREATE TABLE IF NOT EXISTS categories (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(40)  NOT NULL,
+            level INT(6) UNSIGNED,
+            parent_id INT(6) UNSIGNED
+            )";
+
+    if ($connection->query($sql) === TRUE) {
+        echo 'The categories table was created successfully.';
+    } else {
+        echo 'There is a problem with the database, please contact an administrator.';
+    }
+
+    disconnect($connection);
+}
+
+function createIncomesTable()
+{
+    $connection = connectDefaultDatabase();
+
+    $sql = "CREATE TABLE IF NOT EXISTS incomes (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            value FLOAT(6,2) NOT NULL,
+            note VARCHAR(100) NOT NULL,
+            date DATETIME NOT NULL,
+            category_id INT(6) UNSIGNED NOT NULL, 
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+            )";
+
+    if ($connection->query($sql) === TRUE) {
+        echo 'The incomes table was created successfully.';
+    } else {
+        echo 'There is a problem with the database, please contact an administrator.';
+    }
+
+    disconnect($connection);
+}
+
+function dropTable($table)
+{
+    $connection = connectDefaultDatabase();
+
+    $sql = "DROP TABLE $table";
+
+    if ($connection->query($sql) === TRUE) {
+        echo 'The table was dropped successfully.';
+    } else {
+        echo 'There is a problem with the database, please contact an administrator.';
+    }
+
+    disconnect($connection);
+}
---
+++ views/expenses.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+    include("includes/head-tag-content.php");
+    include("includes/logged-in-check.php");
+    ?>
+</head>
+<body>
+<div class="h-100 d-flex align-items-center justify-content-center">
+    <div class="d-flex flex-column card text-center custom-card">
+        <a href="">Expenses</a>
+
+    </div>
+</div>
+<?php include("includes/footer-tag-content.php") ?>
+</body>
+</html>
